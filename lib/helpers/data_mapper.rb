35e0de9ec76668c7efb4160e8881268096b59477
tochman <thomas@nocebo.se>
2016-02-12 13:54:10 +0100

    set up the required entries in the db for Category and User (admin)

--- lib/helpers/data_mapper.rb
+++ lib/helpers/data_mapper.rb
@@ -1,7 +1,18 @@
-
 env = ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://postgres@localhost/slowfood_v1_#{env}")
 DataMapper::Model.raise_on_save_failure = true
 DataMapper.finalize
 #DataMapper.auto_upgrade!
 DataMapper.auto_migrate!
+
+binding.pry
+
+if ENV['RACK_ENV'] != 'test'
+  # Set up the required entries in the db
+  ['Starter', 'Main course', 'Dessert'].each do |category|
+    Category.first_or_create(name: category)
+  end
+
+  User.first_or_create(name: 'Admin', password: 'password', password_confirmation: 'password', admin: true)
+end
+
--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -2,6 +2,6 @@ require './lib/models/category'
 
 describe Category do
   it { is_expected.to have_property :id }
-  it { is_expected.to have_property :name_cat }
+  it { is_expected.to have_property :name }
   it { is_expected.to have_many :dishes }
 end
