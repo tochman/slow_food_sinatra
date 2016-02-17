7f099055f2f26cbc347b6b65c556917f2f406a0a
tochman <thomas@nocebo.se>
2016-02-17 12:26:17 +0100

    fix for failing basket_item spec due to db change

--- spec/basket_item_spec.rb
+++ spec/basket_item_spec.rb
@@ -3,7 +3,7 @@ require './lib/models/basket_item'
 describe BasketItem do
   it { is_expected.to have_property :id }
   it { is_expected.to have_property :qty }
-  it { is_expected.to have_property :dish }
 
+  it { is_expected.to have_one :dish }
   it { is_expected.to belong_to :basket }
 end
