2f16a7e5b1bab197b0a8b9e9d0cfc7cbb8e6b908
tarekalhamad <tarek.alhamad@yahoo.com>
2016-02-25 23:27:05 +0100

    Slow food challange : adding user stories, ER diagram and comments for better understanding

--- lib/models/model.rb
+++ lib/models/model.rb
@@ -1,8 +1,24 @@
 
 class User
   include DataMapper::Resource
+  # Include data mapper to use ORB. To tell DataMapper where to look.
+  # DataMapper only issues updates or creates for the properties it knows about. So it plays well with others.
 
+ # use the datastore's 'pid' field for the id property.
   property :id, Serial, key: true
+  # we can do also this :
+  # use a property called 'uid' as the child key (the foreign key)
+  # belongs_to :user, :child_key => [ :uid ]
+  # to git to items from the database we can use :
+  #1 order_id, item_number = 1, 1
+  #2 LineItem.get(order_id, item_number)
+  #3 => [#<LineItem @orderid=1 @item_number=1>]
+  # auto_migrate! and #auto_upgrade!
+  # While #auto_migrate! destructively drops and recreates tables to match your model definitions,
+  # auto_upgrade! supports upgrading your datastore
+  # In situations where neither #auto_migrate! nor #auto_upgrade! quite cut it,
+  # you can still fall back to the classic migrations feature provided by dm-migrations.
+
   property :username, String, length: 128
 
   property :password, BCryptHash
@@ -15,5 +31,3 @@ class User
     end
   end
 end
-
-
